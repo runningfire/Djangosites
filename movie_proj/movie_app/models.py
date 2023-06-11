01ee589c5bf15bbe3a307c2815485e94fcc8832b
runningfire <Scarr002@mail.ru>
2023-06-11 08:52:47 +0300

    studing

---
+++ movie_proj/movie_app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=40)
+    rating = models.IntegerField()
+    year = models.IntegerField(null=True)
+    budget = models.IntegerField(default=1000000)
+
+    def __str__(self):
+        return f'{self.name} - {self.rating}%'
+
+    #from movie_app.models import Movie
